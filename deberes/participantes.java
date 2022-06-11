0804df9f1e0fc97d819bf1ef4598a8fcdade0301
jose2001P <fjbustillos@gmail.com>
2022-06-11 12:58:34 -0500

    participantes clase

---
+++ deberes/participantes.java
@@ -0,0 +1,20 @@
+/**
+ * Class Untitled
+ */
+public class participantes {
+	private string nombre
+	private string puntuacion
+	}
+
+
+//metodos
+public void ganadores (){
+	System out println("puntaje mas alto");
+}
+public void perdedores (){
+	System out prin ("puntaje mas bajo");
+}
+//contructores
+public participantes (string nombre, string puntuacion){
+    this.nombre = nombre;
+    this.puntuacion = puntuacion; 
