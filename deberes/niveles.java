0826b0ec96aa9bc04c02778485d73b7ffe353f16
jose2001P <fjbustillos@gmail.com>
2022-06-11 12:16:56 -0500

    Carga niveles 2

---
+++ deberes/niveles.java
@@ -0,0 +1,16 @@
+/**
+ * Class Untitled
+ */
+public class niveles {
+	private string nombre
+	private string grado
+	private int espacio
+
+
+//metodos
+public void ficha(){
+	System.out.println("numero de ficha ");
+}
+public void acceder(){
+	System.out.println("accedo al juego");
+}
