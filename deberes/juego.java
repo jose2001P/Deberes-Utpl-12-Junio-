da7b59468526d031e69f58c0cdf9056fe1b4c99f
jose2001P <fjbustillos@gmail.com>
2022-06-11 12:14:30 -0500

    subida 2

---
+++ deberes/juego.java
@@ -0,0 +1,19 @@
+/**
+ * Class Untitled
+ */
+public class juego {
+	private string nombre
+	private string fases
+	private int juego
+
+
+//metodos
+public void entrar(){
+	System.out.println("ingresar.nombre");
+    }
+public void escoger(){
+	System.out.println("escoger personaje del juego");
+	}
+public void salir(){
+	System.out.println("salir del juego ");
+	}
